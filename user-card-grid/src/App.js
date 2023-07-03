b405e129699b43641405b49de999df4829365b39
Hemant Kumbhalkar <hemantkumbhalkar5@gmail.com>
2023-07-04 00:23:40 +0530

    Added loader and some better style

--- user-card-grid/src/App.js
+++ user-card-grid/src/App.js
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import UserCard from "./components/UserCard";
+import { RingLoader } from "react-spinners";
 
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
 
-  const getUsers = async () => {
+  const getUsers = () => {
     setLoading(true);
-    try {
-      const response = await fetch("https://reqres.in/api/users?page=1");
-      const data = await response.json();
-      setUsers(data.data);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
+    setTimeout(async () => {
+      try {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const data = await response.json();
+        setUsers(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }, 1000);
   };
 
   return (
@@ -24,7 +27,9 @@ const App = () => {
       <Navbar getUsers={getUsers} />
       <div className="user-card-grid">
         {loading ? (
-          <div className="loader">Loading...</div>
+          <div className="loader">
+            <RingLoader color={"#4a5c6c"} loading={loading} size={80} />
+          </div>
         ) : (
           users.map((user) => <UserCard key={user.id} user={user} />)
         )}
